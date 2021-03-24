275302a833f5cc786db68ba131d52b10f44435b3
unknown <felipegomescontato8@gmail.com>
2021-03-24 17:04:24 -0300

    Atividade CRUD Back-end minhaLojaDeGames

---
+++ crudgames/src/test/java/com/gegames/crudgames/CrudgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gegames.crudgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
